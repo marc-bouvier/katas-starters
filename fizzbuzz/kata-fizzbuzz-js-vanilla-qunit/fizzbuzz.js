99532e5435b08d19b563d55fae784a8f040093ff
Marc Bouvier <m.bouvier.dev@gmail.com>
2019-02-06 09:17:11 +0100

    Vanilla Js poucant être exécuté directement sur le navigateur

---
+++ fizzbuzz/kata-fizzbuzz-js-vanilla-qunit/fizzbuzz.js
@@ -0,0 +1,3 @@
+fizzbuzz = function(number){
+    return ""
+}
---
+++ fizzbuzz/kata-fizzbuzz-js-vanilla-qunit/tests.js
@@ -0,0 +1,3 @@
+QUnit.test( "fizzbuzz 1", function( assert ) {
+    assert.equal( fizzbuzz(1), "1" , 'fizzbuzz de 1 devrait être "1"');
+  });
